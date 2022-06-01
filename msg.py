0925d733645682e3046de5d29ddf1cc611812352
AdrianoVolter <adriano.j.volter@hotmail.com>
2022-06-01 14:35:21 -0300

    Criando botão no tkinter , função para subir uma imagem docker

--- msg.py
+++ msg.py
@@ -2,18 +2,16 @@ import os
 from tkinter import *
 from tkinter import messagebox
 
-#cont = os.system('docker run -d -p 80:80 docker/getting-started')
-
-
 app = Tk()
 
 def myClick():
-      myLabel = Label(app, text='Esta OK!' )
-      myLabel.pack()
-
-myButton = Button(app, text='Clique aqui!\nInstalar arquivo', command=myClick ,pady=50,fg='red')
+      os.system('docker run -d -p 80:80 docker/getting-started')
+      
+myButton = Button(app, text='Clique aqui!\nInstalar arquivo', command=myClick ,pady=50,fg='red', bg='blue')
 myButton.pack()
 app.title('ARQUIVO-1',)
+
+#Caixa de mensagem na tela.
 #app.geometry('500x300')
 #messagebox.showinfo(title='ARQUIVO-1', message=
 #'CONTAINER UP!')
