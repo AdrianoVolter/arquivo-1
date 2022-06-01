f9c2d5007c5975ec8150bb5b05b12d038a2b9b78
AdrianoVolter <adriano.j.volter@hotmail.com>
2022-06-01 18:00:45 -0300

    Script executavel , com botao do tkinter ,

--- msg.py
+++ msg.py
@@ -1,16 +1,17 @@
 import os
 from tkinter import *
-from tkinter import messagebox
+from time import sleep
 
 app = Tk()
-
 def myClick():
       os.system('docker run -d -p 80:80 docker/getting-started')
+      sleep(5)
+      app.destroy()
       
-myButton = Button(app, text='Clique aqui!\nInstalar arquivo', command=myClick ,pady=50,fg='red', bg='blue')
-myButton.pack()
-app.title('ARQUIVO-1',)
+myButton1 = Button(app, text='INSTALAR PROGRAMA\nClique aqui', command=myClick ,padx=40,pady=40,fg='white', bg='red')
+myButton1.pack()
 
+app.title('ARQUIVO-1',)
 
 #app.geometry('500x300')
 #messagebox.showinfo(title='ARQUIVO-1', message=
